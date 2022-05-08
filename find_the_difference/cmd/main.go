a2167607e6cb7029d343511ba5dcd3af54af1fdd
sardortoshkentov <toshkentovsardor.2003@gmail.com>
2022-05-08 21:05:59 +0500

    Find the Difference problem solved

---
+++ find_the_difference/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"find_the_difference/source"
+	"fmt"
+)
+
+func main() {
+	result := source.FindTheDifference("abcd", "abcde")
+	fmt.Println("First Result: ", string(result)) // e
+
+	result = source.FindTheDifference("a", "aa")
+	fmt.Println("Second Result: ", string(result)) // a
+}
