6a81a4c4304da6604cdc77b565c06933b9f42f40
s.toshkentov <toshkentovsardor.2003@gmail.com>
2023-10-31 19:13:10 +0500

    climb stairs optimized

--- climb_stairs/climb_stairs.go
+++ climb_stairs/climb_stairs.go
@@ -1,21 +1,19 @@
 package climbstairs
 
 func climbStairs(n int) int {
-	if n == 1 {
+	if n <= 1 {
 		return 1
 	}
 
-	dp := make([]int, n+1)
+	var result int
 
-	if len(dp) < 2 {
-		return len(dp)
+	a, b := 1, 1
+
+	for i := 2; i <= n; i++ {
+		result = a + b
+		a, b = b, result
 	}
 
-	dp[1], dp[2] = 1, 2
-
-	for i := 3; i <= n; i++ {
-		dp[i] = dp[i-2] + dp[i-1]
-	}
-
-	return dp[n]
+	return result
 }
+
