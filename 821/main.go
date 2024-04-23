59c4c2d53fe599fa2ca481113d4755437b19fef6
Sardor Toshkentov <toshkentovsardor.2003@gmail.com>
2024-04-23 19:30:13 +0500

    821 solved

---
+++ 821/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+func shortestToChar(s string, c byte) []int {
+	indexSlice := make([]int, 0, len(s))
+
+	result := make([]int, len(s))
+
+	for i := range s {
+		if s[i] == c {
+			indexSlice = append(indexSlice, i)
+		}
+	}
+
+	var (
+		counter        int
+		lastEqualIndex int
+		flag           bool
+	)
+
+	for i := range s {
+		if s[i] != c {
+			if !flag {
+				result[i] = indexSlice[counter] - i
+				continue
+			}
+
+			distance := indexSlice[counter] - i
+			if distance < 0 {
+				distance = distance * -1
+			}
+
+			if abs(lastEqualIndex, i) >= distance {
+				result[i] = distance
+				continue
+			}
+
+			result[i] = abs(lastEqualIndex, i)
+			continue
+		}
+
+		lastEqualIndex = i
+		flag = true
+		result[i] = 0
+
+		if counter+1 != len(indexSlice) {
+			counter++
+		}
+	}
+
+	return result
+}
+
+func abs(a, b int) int {
+	fmt.Println(a, b)
+	if a-b < 0 {
+		return (a - b) * -1
+	}
+
+	return a - b
+}
